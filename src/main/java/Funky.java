0e325b48e881e67dff62159cfb10ec4fa9ecbb15
Yong Xiang <yx0622@gmail.com>
2026-08-27 23:21:22 +0800

    Changed name and added hello and bye

---
+++ src/main/java/Funky.java
@@ -0,0 +1,20 @@
+public class Funky {
+    public static void main(String[] args) {
+        String banner = " _____             _          \n"
+                + "|  ___|   _ _ __  | | ___   _ \n"
+                + "| |_ | | | | '_ \\ | |/ / | | |\n"
+                + "|  _|| |_| | | | ||   <| |_| |\n"
+                + "|_|   \\__,_|_| |_||_|\\_\\\\__, |\n"
+                + "                         |___/ \n";
+
+
+        System.out.println(banner);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Funky");
+        System.out.println("What can I do for you? ");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
