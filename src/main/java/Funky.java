9cb5e210758d4f3522398d4c1f7883cad94aeca5
Yong Xiang <yx0622@gmail.com>
2026-08-27 23:47:19 +0800

    Echo (Level 1)

--- src/main/java/Funky.java
+++ src/main/java/Funky.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Funky {
+
+
+
     public static void main(String[] args) {
         String banner = " _____             _          \n"
                 + "|  ___|   _ _ __  | | ___   _ \n"
@@ -8,11 +13,25 @@ public class Funky {
                 + "                         |___/ \n";
 
 
+
+        // BANNER LEVEL 0
         System.out.println(banner);
+
+        // INTRODUCTION //LEVEL 0
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Funky");
         System.out.println("What can I do for you? ");
         System.out.println("____________________________________________________________");
+
+        // ECHO LEVEL 1
+        Scanner in = new Scanner(System.in);
+        String echo = in.nextLine();
+        System.out.println("____________________________________________________________");
+        System.out.println(echo);
+        System.out.println("____________________________________________________________");
+
+
+        //GOODBYE LEVEL 0
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
 
